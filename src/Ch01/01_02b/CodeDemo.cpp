503be3e9d5d18e9e6fe90633bc77e21e0ec32e66
naruto-itachi1 <benakatm36@gmail.com>
2025-07-24 17:49:38 +0000

    Add initial code demos for exercises 00_03, 01_02, and 01_09

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,9 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    std::cout <<" Konichiwa " << std::endl;
+}
